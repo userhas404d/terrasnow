23b7f41933a273dcb6373db8c52c8bbcba719f13
userhas404d <29389186+userhas404d@users.noreply.github.com>
2018-04-04 11:36:27 -0400

    remove outputs.tf from cat item var creation

--- terrasnow.py
+++ terrasnow.py
@@ -55,7 +55,6 @@ def unzip_and_create_vars(user_name, user_pwd, file_name, cat_sys_id):
     my_zip = zip_handler.zip_parser(file_name, file_path, cat_sys_id)
     my_zip.unzip()
     my_zip.hcl_to_json(my_zip.tf_var_loc)
-    my_zip.hcl_to_json(my_zip.tf_out_loc)
     var_list = my_zip.json_to_servicenow()
 
     # push category item variables to snow
