dafe3f77bc870f48a918a95e79ac97b1faf65e2c
userhas404d <29389186+userhas404d@users.noreply.github.com>
2018-04-09 12:58:01 -0400

    moved wait operation to server side script and added loading modal to client script

--- script-includes/getAWSinfo_client.js
+++ script-includes/getAWSinfo_client.js
@@ -1,16 +1,85 @@
-//reference: https://community.servicenow.com/community?id=community_question&sys_id=b0ff4ba5dbdcdbc01dcaf3231f961989
-// new GlideAjax object referencing name of AJAX script include
-var ga = new GlideAjax("TerraformAWSAccountQuery");
-// add name parameter to define which function we want to call
-// method name in script include will be getFavorites
-ga.addParam("sysparm_name", "getAWSAccountInfo");
-// submit request to server, call ajaxResponse function with server response
-ga.getXMLWait();
-var resp = ga.getAnswer();
-resp = JSON.parse(resp); // Parse the JSON string
-alert(resp.message);
-for (var i = 0; i < resp.favorites.length; i++) {
+function onLoad() {
+ function getAWSAccountInfo(){
+	var ga = new GlideAjax("TerraformAWSAccountQuery");
+	// add name parameter to define which function we want to call
+	// method name in script include will be getFavorites
+	ga.addParam("sysparm_name", "getAWSAccountInfo");
+	// submit request to server, call ajaxResponse function with server response
+	ga.getXMLWait();
+	var workflowSysId = ga.getAnswer();
+	jslog("getAwsAccountInfo response: " + workflowSysId);
+	var workflowGlideRecord = getWorkflowGlideRecord(workflowSysId);
+	var jsonObj = getWorkflowResults(workflowGlideRecord);
+	return jsonObj;
+ }
 
-       alert(resp.favorites[i].key + ': ' + resp.favorites[i].value);
+function getWorkflowGlideRecord(sysId){
+	var gr = new GlideRecord('wf_context');
+		 gr.addQuery('sys_id', sysId);
+		 gr.query();
+			while(gr.next()){
+					return gr;
+			   }
+}
+
+function getWorkflowResults(workflowGlideRecord){
+   			var result = workflowGlideRecord.scratchpad;
+//   			jslog('json_obj: ' + result);
+        //convert scratchpad string result to json obj
+   			var test = JSON.parse(result);
+//         //extract 'aws account info' as json string from scratchpad json obj
+  			var json_str = test['json_obj'];
+//   			jslog("result json: " +json_str);
+//         //convert 'aws account info' json string into json obj
+  	         var json_obj = JSON.parse(json_str);
+//         //pass json obj to set_values function
+	return json_obj;
+}
+
+function setValues(json_obj){
+   //Type appropriate comment here, and begin script below
+   jslog(typeof json_obj);
+   jslog('made it this far');
+   jslog('json_obj: '+json_obj);
+   for (var list in json_obj['amis']){
+   //gs.log(list);
+     for (var key in json_obj['amis'][list]){
+  	   if (json_obj['amis'][list][key]['ImageId']){
+  	   g_form.addOption('tfv_AmiId', json_obj['amis'][list][key]['ImageId'],json_obj['amis'][list][key]['Name']);
+  	   g_form.addOption('tfv_AmiDistro', json_obj['amis'][list][key]['OSType'], json_obj['amis'][list][key]['OSType'] );
+  	       }
+       }
+  }
+
+  for (var key0 in json_obj['key_pairs']){
+  	if(json_obj['key_pairs'][key0]['KeyName']){
+  	   g_form.addOption('tfv_KeyPairName',json_obj['key_pairs'][key0]['KeyName'], json_obj['key_pairs'][key0]['KeyName']);
+  	}
+  }
+
+  for (var key1 in json_obj['vpcs']){
+  	if(json_obj['vpcs'][key1]['Name']){
+  		g_form.addOption('tfv_SubnetId',json_obj['vpcs'][key1]['VpcId'], json_obj['vpcs'][key1]['Name']);
+  	}
+  }
+
+  for (var key2 in json_obj['security_groups']){
+  		if(json_obj['security_groups'][key2]['Name']){
+  		    g_form.addOption('tfv_SecurityGroupIds', json_obj['security_groups'][key2]['GroupId'], json_obj['security_groups'][key2]['Name']);
+  	  }
+  }
+}
+
+function showLoadingOverlay(){
+	showLoadingDialog();
+	setTimeout(function() {
+		setValues(getAWSAccountInfo());
+		hideLoadingDialog();
+	}, 20000);
+}
+
+//Show the loading dialog immediately as the form loads
+showLoadingOverlay();
+// var json_str = waitForElement(sys_id);
 
 }
--- script-includes/getAWSinfo_server.js
+++ script-includes/getAWSinfo_server.js
@@ -10,12 +10,41 @@ TerraformAWSAccountQuery.prototype = Object.extendsObject(AbstractAjaxProcessor,
 	 * awsAccountValues.addParam("sysparm_name","getAWSAccountInfo");
 	 */
 	getAWSAccountInfo: function() { // build new response xml element for result
-        var result = { favorites: [], message: '' };
-        result.message = "returning all favorites";
-        result.favorites.push({ 'key': 'color', 'value': 'blue'});
-        result.favorites.push({ 'key': 'beer', 'value': 'lager'});
-        result.favorites.push({ 'key': 'pet', 'value': 'dog'});
-        return new JSON().encode(result); // Encodes result object as JSON string and pipes it to the XML answer
+
+		var wf = new Workflow();
+		var wfId = wf.getWorkflowFromName("tfsnow - get aws account info");
+		gs.log('TerraformAWSAccountQuery - workflow sysId: ' + wfId);
+		var context = wf.startFlow(wfId, null, "tfsnow - get aws account info", null);
+		var sysId = context.sys_id;
+		gs.log('TerraformAWSAccountQuery - workflow context sysId: ' + sysId);
+		this._waitForElement(sysId);
+		return sysId;
       },
+
+    _getWorkflowGlideRecord: function(sysId) {
+			 // gs.log('TerraformAWSAccountQuery - _getWorkflowState: made it this far' );
+ 			 var gr = new GlideRecord('wf_context');
+ 			 gr.addQuery('sys_id', sysId);
+ 			 gr.query();
+ 			    while(gr.next()){
+ 					    return gr;
+ 				   }
+				 },
+
+    _waitForElement: function(sysId) {
+	    var gr = this._getWorkflowGlideRecord(sysId);
+		var workflowState = gr.state;
+	    if(typeof workflowState == "undefined" || workflowState == null || workflowState == 'executing'){
+	        gs.log('TerraformAWSAccountQuery - _waitForElement: workflow state is executing..');
+			//wait for 5 seconds and then try again..
+			gs.sleep(5000);
+			this._waitForElement(sysId);
+	    }
+	    else{
+			gs.log('TerraformAWSAccountQuery - _waitForElement: WORKFLOW COMPLETE!');
+  			return sysId;
+	    }
+	  },
+
       type : "TerraformAWSAccountQuery"
    });
