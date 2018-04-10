b6eec3e01d4d7ba3f27c22b2c66283eecb91c745
userhas404d <29389186+userhas404d@users.noreply.github.com>
2018-04-10 13:58:44 -0400

    retrieve text for client scripts directly from javascript files

--- handlers/sn_client_script_handler.py
+++ handlers/sn_client_script_handler.py
@@ -1,5 +1,7 @@
 """Handle client script creations."""
 
+import os
+
 
 class SnowClientScript(object):
     """Terraform Zip Parser."""
@@ -8,133 +10,20 @@ class SnowClientScript(object):
         """Initialize."""
         self.cat_item_id = cat_item_id
         self.client_script_list = []
-        self.display_toggle_script = """
-        function onChange(control, oldValue, newValue, isLoading) {
-           if (isLoading || newValue == '') {
-              return;
-           }
-           //Type appropriate comment here, and begin script below
-           if(newValue == 'true'){
-            for (var index = 0; index < g_form.nameMap.length; index++) {
-                var unhideme = g_form.nameMap[index].prettyName;
-                if(!(unhideme.startsWith('gen_')) && !(g_form.isMandatory(unhideme))){
-                    g_form.setDisplay(unhideme,true);
-                    }
-                }
-            }
-            else{
-                for (var i = 0; i < g_form.nameMap.length; i++) {
-                    var hideme = g_form.nameMap[i].prettyName;
-                    if(hideme.startsWith('gen_') || (!(g_form.isMandatory(hideme)) && hideme != 'adv_toggle')){
-                        g_form.setDisplay(hideme,false);
-                        }
-                    }
-                }
+        self.sn_javascript_path = '../sn_javascript/'
+        self.display_toggle_script = (
+          self.getJavascriptText('createDisplayToggleOnload.js'))
+        self.hide_generic_vars_script = (
+          self.getJavascriptText('hideGenericVariablesOnLoad.js'))
+        self.dropdown_population_script = (
+          self.getJavascriptText('populateDropdownsOnLoad.js'))
 
-        }
-        """
-        self.hide_generic_vars_script = """
-        function onLoad() {
-            for (var index = 0; index < g_form.nameMap.length; index++) {
-                var hideme = g_form.nameMap[index].prettyName;
-                if(hideme.startsWith('gen_') || (!(g_form.isMandatory(hideme)) && hideme != 'adv_toggle')){
-                    g_form.setDisplay(hideme,false);
-                    }
-                }
-            }
-        """
-        self.dropdown_population_script = """
-        function onLoad() {
-        function getAWSAccountInfo(){
-            var ga = new GlideAjax("TerraformAWSAccountQuery");
-            // add name parameter to define which function we want to call
-            // method name in script include will be getFavorites
-            ga.addParam("sysparm_name", "getAWSAccountInfo");
-            // make syncronus GlideAjax call
-            ga.getXMLWait();
-            var workflowSysId = ga.getAnswer();
-            jslog("getAwsAccountInfo response: " + workflowSysId);
-            var workflowGlideRecord = getWorkflowGlideRecord(workflowSysId);
-            var jsonObj = getWorkflowResults(workflowGlideRecord);
-            return jsonObj;
-        }
-
-        function getWorkflowGlideRecord(sysId){
-            var gr = new GlideRecord('wf_context');
-            gr.addQuery('sys_id', sysId);
-            gr.query();
-            while(gr.next()){return gr;}
-        }
-
-        function getWorkflowResults(workflowGlideRecord){
-            var result = workflowGlideRecord.scratchpad;
-         // jslog('json_obj: ' + result);
-            // convert scratchpad string result to json obj
-            var test = JSON.parse(result);
-            // extract 'aws account info' as json string from scratchpad json obj
-            var json_str = test['json_obj'];
-         // jslog("result json: " +json_str);
-         // convert 'aws account info' json string into json obj
-            var json_obj = JSON.parse(json_str);
-         // pass json obj to set_values function
-            return json_obj;
-        }
-
-        function setValues(json_obj){
-           //Type appropriate comment here, and begin script below
-           // jslog(typeof json_obj);
-           // jslog('made it this far');
-           // jslog('json_obj: '+json_obj);
-           g_form.addOption('tfv_AmiId', '-- None --');
-           g_form.addOption('tfv_AmiDistro', '-- None --');
-           g_form.addOption('tfv_KeyPairName', '-- None --');
-           g_form.addOption('tfv_SecurityGroupIds', '-- None --');
-           g_form.addOption('tfv_SubnetId', '-- None --');
-           for (var list in json_obj['amis']){
-           //gs.log(list);
-             for (var key in json_obj['amis'][list]){
-                 if (json_obj['amis'][list][key]['ImageId']){
-                 g_form.addOption('tfv_AmiId', json_obj['amis'][list][key]['ImageId'],json_obj['amis'][list][key]['Name']);
-                 g_form.addOption('tfv_AmiDistro', json_obj['amis'][list][key]['OSType'], json_obj['amis'][list][key]['OSType'] );
-             }
-           }
-         }
-
-          for (var key0 in json_obj['key_pairs']){
-              if(json_obj['key_pairs'][key0]['KeyName']){
-                  g_form.addOption('tfv_KeyPairName',json_obj['key_pairs'][key0]['KeyName'], json_obj['key_pairs'][key0]['KeyName']);
-            }
-          }
-
-          for (var key1 in json_obj['vpcs']){
-              if(json_obj['subnets'][key1]['Name']){
-              g_form.addOption('tfv_SubnetId',json_obj['subnets'][key1]['SubnetId'], json_obj['subnets'][key1]['Name']);
-            }
-          }
-
-          for (var key2 in json_obj['security_groups']){
-              if(json_obj['security_groups'][key2]['Name']){
-                g_form.addOption('tfv_SecurityGroupIds', json_obj['security_groups'][key2]['GroupId'], json_obj['security_groups'][key2]['Name']);
-              }
-          }
-        }
-
-    function showLoadingOverlay(){
-        showLoadingDialog();
-        setTimeout(function() {
-            setValues(getAWSAccountInfo());
-            hideLoadingDialog();
-        }, 1000);
-    }
-
-//Show the loading dialog immediately as the form loads
-showLoadingOverlay();
-// var json_str = waitForElement(sys_id);
-
-
-}
-"""
-
+    def getJavascriptText(self, target_file):
+        """Return text of the target file."""
+        jspath = os.path.abspath(os.path.relpath(self.sn_javascript_path))
+        with open(jspath + '/' + target_file) as file:
+            script_text = file.read()
+        return script_text
 
     def create_display_toggle(self):
         """Create the dispaly toggle client script."""
