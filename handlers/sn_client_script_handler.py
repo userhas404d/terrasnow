926453f8e56ad4d3bb481b9788d0c0a6553b8687
userhas404d <29389186+userhas404d@users.noreply.github.com>
2018-04-11 09:24:34 -0400

    add filterSubnetsOnChange client script and streamline sn_client_script_handler

--- handlers/sn_client_script_handler.py
+++ handlers/sn_client_script_handler.py
@@ -12,12 +12,6 @@ class SnowClientScript(object):
         self.cat_item_id = cat_item_id
         self.client_script_list = []
         self.sn_javascript_path = '/sn_javascript/'
-        self.display_toggle_script = (
-          self.getJavascriptText('createDisplayToggleOnload.js'))
-        self.hide_generic_vars_script = (
-          self.getJavascriptText('hideGenericVariablesOnLoad.js'))
-        self.dropdown_population_script = (
-          self.getJavascriptText('populateDropdownsOnLoad.js'))
 
     def getJavascriptText(self, target_file):
         """Return text of the target file."""
@@ -31,55 +25,58 @@ class SnowClientScript(object):
             print('ERROR')
             raise
 
-    def create_display_toggle(self):
-        """Create the dispaly toggle client script."""
+    def create_client_script(self, name, script_type, cat_var, script):
+        """Create client script data block from REST call."""
         self.client_script_list.append(
              {
                  "active": "true",
-                 "name": "display toggle",
+                 "name": name,
                  "applies_to": "A Catalog item",
                  "ui_type": "0",
-                 "type": "onChange",
+                 "type": script_type,  # onChange, onLoad, onUpdate
                  "cat_item": self.cat_item_id,
-                 "cat_variable": "adv_toggle",
+                 "cat_variable": cat_var,
                  "applies_catalog": "true",
                  "applies_sc_task": "true",
-                 "script": self.display_toggle_script
+                 "script": script
              })
+
+    def create_display_toggle(self):
+        """Create the dispaly toggle client script."""
+        self.create_client_script(name="display toggle",
+                                  script_type="onChange",
+                                  cat_var="adv_toggle",
+                                  script=self.getJavascriptText(
+                                   'createDisplayToggleOnload.js'))
 
     def create_hide_generic_vars(self):
         """Create the dispaly toggle client script."""
-        self.client_script_list.append(
-             {
-                 "active": "true",
-                 "name": "hide generic vars",
-                 "applies_to": "A Catalog item",
-                 "ui_type": "0",
-                 "type": "onLoad",
-                 "cat_item": self.cat_item_id,
-                 "applies_catalog": "true",
-                 "applies_sc_task": "true",
-                 "script": self.hide_generic_vars_script
-             })
+        self.create_client_script(name="hide generic vars",
+                                  script_type="onLoad",
+                                  cat_var="",
+                                  script=self.getJavascriptText(
+                                    'hideGenericVariablesOnLoad.js'))
 
     def create_population_dropdown(self):
         """Create the dispaly toggle client script."""
-        self.client_script_list.append(
-             {
-                 "active": "true",
-                 "name": "populate dropdowns",
-                 "applies_to": "A Catalog item",
-                 "ui_type": "0",
-                 "type": "onLoad",
-                 "cat_item": self.cat_item_id,
-                 "applies_catalog": "true",
-                 "applies_sc_task": "true",
-                 "script": self.dropdown_population_script
-             })
+        self.create_client_script(name="populate dropdowns",
+                                  script_type="onLoad",
+                                  cat_var="",
+                                  script=self.getJavascriptText(
+                                    'populateDropdownsOnLoad.js'))
+
+    def create_subnet_filter(self):
+        """Create the dispaly toggle client script."""
+        self.create_client_script(name="populate dropdowns",
+                                  script_type="onLoad",
+                                  cat_var="tfv_SecurityGroupIds",
+                                  script=self.getJavascriptText(
+                                    'filterSubnetsOnChange.js'))
 
     def get_scripts(self):
         """Preform correct order of operations and return variables."""
         self.create_display_toggle()
         self.create_hide_generic_vars()
         self.create_population_dropdown()
+        self.create_subnet_filter()
         return self.client_script_list
