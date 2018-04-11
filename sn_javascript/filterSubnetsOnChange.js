f9648ec52c1fead9ae43a0e7f0cea6dc0922b55f
userhas404d <29389186+userhas404d@users.noreply.github.com>
2018-04-11 09:36:37 -0400

    comment out alerts

--- sn_javascript/filterSubnetsOnChange.js
+++ sn_javascript/filterSubnetsOnChange.js
@@ -4,17 +4,17 @@ function onChange(control, oldValue, newValue, isLoading) {
    }
    var json_obj = JSON.parse(g_form.getValue('gen_AwsAccountInfo'));
    g_form.clearOptions('tfv_SubnetId');
-   alert("oldValue: " + oldValue + "newValue: " + newValue);
+   //alert("oldValue: " + oldValue + "newValue: " + newValue);
    for (var sg in json_obj['security_groups']){
 		   if(json_obj['security_groups'][sg]['GroupId'] == newValue){
 			   vpcId = json_obj['security_groups'][sg]['VpcId'];
-			   alert('found vpc: ' + vpcId);
+			   //alert('found vpc: ' + vpcId);
 		   }
 	   }
    for (var subnet in json_obj['subnets']){
     if(json_obj['subnets'][subnet]['VpcId'] == vpcId){
         g_form.addOption('tfv_SubnetId',json_obj['subnets'][subnet]['SubnetId'], json_obj['subnets'][subnet]['Name']);
-		g_form.setDisabled('tfv_SubnetId', false);
+		    g_form.setDisabled('tfv_SubnetId', false);
       }
     }
 }
